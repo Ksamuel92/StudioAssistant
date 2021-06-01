9c63a3b51e4d98ff329b5a60063fec94d5e4cd7d
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-01 19:25:21 -0400

    finish seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,13 @@ RecordingSession.find_or_create_by(:start_date => "01/09/2021", :end_date => "#{
 RecordingSession.find_or_create_by(:start_date => "01/10/2021", :end_date => "#{Faker::Date.in_date_period}", :per_hour => "#{Faker::Number.number(digits:2)}", :hours_per_day => "#{Faker::Number.number(digits:1)}")
 
 
-Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
+Client.create(:name => "#{Faker::Name.name}", :email => "#{Faker::Internet.email}", :band? =>"#{Faker::Boolean.boolean}", :has_producer? => "#{Faker::Boolean.boolean}", :budget => "1500.24", :genre => "#{Faker::Music.genre}")
