62e6a5db28a39171d2726280908378a3eaf9d10e
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-01 18:40:31 -0400

    testing database, starting seed file

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ActiveRecord::Base
     has_many :recording_sessions
     has_many :users, through: :recording_sessions
+    validates :name, :band?, :genre, :has_producer?, :budget, presence: true
+    validates :email, presence: true, uniqueness: true #downcase emails
 end
