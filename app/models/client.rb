3fcadbc8c59c3f2dd878996dccbdd3e5f141318c
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-03 11:58:48 -0400

    work on client model and session new view

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base
     has_many :recording_sessions
     has_many :users, through: :recording_sessions
-    validates :name, :band?, :genre, :has_producer?, :budget, presence: true
+    validates :name, presence: true
     validates :email, presence: true, uniqueness: true #downcase emails
 end
