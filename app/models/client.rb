770e31f185197c9a2e73fdffc31e6cdfa20bc1e0
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-04 12:36:20 -0400

    add slug methods to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,17 @@ class Client < ActiveRecord::Base
     has_many :users, through: :recording_sessions
     validates :name, presence: true
     validates :email, presence: true#downcase emails
+
+
+    def find_by_slug(slug)
+        self.all.find{|i| i.slug == slug}
+    end
+
+    def slug
+        self.user.name.downcase.gsub(" ","-")
+    end
+  
+
+
+
 end
