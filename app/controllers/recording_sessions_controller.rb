8225a8cb0116669f0c6efd3e46bc5c0d64351d3e
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-05 10:54:15 -0400

    cleaned up recordingsessions controller

--- app/controllers/recording_sessions_controller.rb
+++ app/controllers/recording_sessions_controller.rb
@@ -2,9 +2,7 @@ class RecordingSessionsController < ApplicationController
 
   # GET: /recording_sessions
   get "/recordingsessions" do
-    if !is_logged_in?
-      redirect to '/login'
-    end
+    validate_login
     current_user
     @recording_sessions = current_user.recording_sessions
     # binding.pry
@@ -22,17 +20,13 @@ class RecordingSessionsController < ApplicationController
 
   # GET: /recording_sessions/new
   get "/recordingsessions/new" do
+    validate_login
     #grabs form to create a new recording session
     erb :"/recording_sessions/new.html"
   end
 
   # POST: /recording_sessions
   post "/recordingsessions" do
-    # binding.pry
-  #  if params[:recording_sessions][:start_date] = ""
-    # redirect to "/recordingsessions/new"
-    #error message
-  #  end
     remove_comma_from_integer(params[:client][:budget])
     current_user
     client = Client.new(params[:client])
@@ -42,7 +36,6 @@ class RecordingSessionsController < ApplicationController
       redirect to "/recordingsessions/new"
       #error message
     else
-
       user.recording_sessions << recording_session
       client.recording_sessions << recording_session
       # user.clients << client
@@ -55,50 +48,46 @@ class RecordingSessionsController < ApplicationController
 
   # GET: /recording_sessions/5
   get "/recordingsessions/:slug/:id" do
-   
-  current_user
-  find_session_and_client
+    validate_login
+    current_user
+    set_recording_session_and_client
     erb :"/recording_sessions/show.html"
   end
 
   # GET: /recording_sessions/5/edit
   get "/recordingsessions/:slug/:id/edit" do
-  if !is_logged_in?
-      redirect to '/login'
-    end
-    @user = current_user
-    @recording_session = RecordingSession.find_by_id(params[:id])
-    @client = Client.find_by_slug(params[:slug])
+    validate_login
+    current_user
+    set_recording_session_and_client
     erb :"/recording_sessions/edit.html"
   end
 
   # PATCH: /recording_sessions/5
   patch "/recordingsessions/:slug/:id" do
     # binding.pry
-    recording_session = RecordingSession.find_by_id(params[:id])
-    client = Client.find_by_slug(params[:slug])
-
-    recording_session.update(params[:recording_session])
-    client.update(params[:client])
-    # @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-    # @song.genre_ids = params[:genres]
-    recording_session.save
-    client.save
-    redirect "/recordingsessions/#{client.slug}/#{recording_session.id}"
+    set_recording_session_and_client
+    @recording_session.update(params[:recording_session])
+    @client.update(params[:client])
+    @recording_session.save
+    @client.save
+    redirect "/recordingsessions/#{@client.slug}/#{@recording_session.id}"
   end
 
 
   delete "/recordingsessions/:slug/:id/delete" do
-    # binding.pry
-    recording_session = RecordingSession.find_by_id(params[:id])
-    client = Client.find_by_slug(params[:slug])
-
-    recording_session.destroy
-    client.destroy
+    set_recording_session_and_client
+    @recording_session.destroy
+    @client.destroy
     redirect "/recordingsessions"
   end
 
   private
+
+  def validate_login
+    if !is_logged_in?
+      redirect to '/login'
+    end
+  end
 
   def set_recording_session_and_client
     @recording_session = RecordingSession.find_by_id(params[:id])
