3f63d0b4a2ebf953d7b1d105e2724cbf3e15f089
Kyle Samuel <Ksamuel92@gmail.com>
2021-06-09 16:25:31 -0400

    add recording_session model functionality

--- app/models/recording_session.rb
+++ app/models/recording_session.rb
@@ -2,23 +2,20 @@ class RecordingSession < ActiveRecord::Base
     #model associations
     belongs_to :user
     belongs_to :client
-    # validates :start_date, presence: true
-    #model validations
-    #check bookmarks for help validating dates
-    # validates :start_date, :end_date, presence: true
-    # validate :end_date_after_start_date
-    # validates :email, presence: true
-    # def self.find_by_slug(slug)
-    #     self.all.find{|i| i.slug == slug}
-    # end
 
-    # def slug
-    #     self.user.name.downcase.gsub(" ","-")
-    # end
-  
+    def amount_of_money_per_day
+        per_hour * hours_per_day
+    end
 
+    def days_to_work
+        (budget)/(per_hour * hours_per_day).floor
+    end
 
-    # def self.parse_date(date)
-    #     Date.strptime(date, '%m/%d/%Y')
-    # end
+    def days_to_work_message
+        if days_to_work == 1
+        "#{days_to_work} day."
+        else
+        "#{days_to_work} days."
+        end
+    end
 end
